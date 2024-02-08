3a36ee44443c134cce00f1ff2dadd5981d5515e2
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-08 09:26:02 +0000

    8.2.24 09:25

--- week02/bank.py
+++ week02/bank.py
@@ -1,8 +1,10 @@
 # bank.py Week 02 Task
-# The aim of this program is to prompt the user and read in two money amounts (in cents), add the two ammounts and print the answer.
+# The aim of this program is to prompt the user and read in two money amounts (in cents), 
+# add the two ammounts and print the answer.
 # Author: Laura Lyons
 
-x = int(input('Enter ammount 1 (in cents): ')) 
+x = int(input('Enter ammount 1 (in cents): ')) # input reads in a string so we need to convert to an integer
+# to allow us to perform mathematical operations.   
 y = int(input('Enter ammount 2 (in cents): '))
 answer= ((x+y)/100) # By dividing by 10, i will convert from cents to euro
 print (f'The sum of these is: \N{euro sign}{answer}')
