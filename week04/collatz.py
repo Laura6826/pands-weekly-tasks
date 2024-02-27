b40e59fcbe9bd10882f78b8c5c2d6f6c9363ccf3
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-27 10:47:46 +0000

    updated references

--- week04/collatz.py
+++ week04/collatz.py
@@ -3,23 +3,26 @@
 # following the application of specific calculations
 # If the integer is even, divide it by two
 # if the integer is odd, multiple it by three and add one.
+
 # Author: Laura Lyons
 
 
-
 def collatz_sequence (x):
-    sequence = [x] #Reference Copilot (AI) 
-    while x!= 1:
-        if (x % 2) ==0:
+    sequence = [x]              # Reference: O'Reilly (2015) A Whirlwind Tour of Python (pg:8)
+    while x!= 1:                # while 'x' is not equal to 1,
+        if (x % 2) ==0:         # If 'x' is divided by 2 and the answer is 0, the number entered is an even number. 
+                                # Proceed to divide the number by 2.
             x= x //2
+            sequence.append(x)  # Append 'sequence', to refect the calculation applied.
+                                # Reference: O'Reilly (2015) A Whirlwind Tour of Python (pg:16)
         else:
-            x = (x*3) +1
-            sequence.append(x)
+            x = (x*3) +1        # If, when 'x' is divided by 2 and the answer is not 0, the number entered must be an odd number.
+                                # Proceed to multiply the entered number by 3 and then add 1. 
+            sequence.append(x)  # Append 'sequence', to refect the calculation applied.
     return sequence
        
 
-guess= int(input('Please enter a positive integer: '))
-# We need into check that the value entered is a positive integer
+guess= int(input('Please enter a positive integer: ')) # We need into check that the value entered is a positive integer
 while guess <= 0:
     guess = int(input('That is not a positive integer. Please enter a positive integer:'))
 else:
