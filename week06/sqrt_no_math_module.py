1f4f798249906fc5055368a99d6ea6f5aedf95d5
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-02 22:53:07 +0000

    weekly Task 06

--- week06/sqrt_no_math_module.py
+++ week06/sqrt_no_math_module.py
@@ -4,28 +4,24 @@
 # Author: Laura Lyons
 
 # Newtons method for square roots, is a technique for finding the square root of a number by iteratively improving an initial guess.
+# The formula for Newtons method = square_root (original_number)= (((original_number/estimated_sqrt)+ estimated_sqrt)/2)
 # References: python - Finding the square root using Newton's method (errors!) - Stack Overflow ()
 
-# The formula for Newtons method = square_root (original_number)= (((original_number/estimated_sqrt)+ estimated_sqrt)/2)
-
-import math as m # this will only be used as a check, to see how accurate our formula is.
-
-# application of the Newton Method for square roots formula was extropolated from 
+# The application of the Newton Method for square roots formula was extropolated from 
 # https://stackoverflow.com/questions/70793490/how-do-i-calculate-square-root-in-python. 
 
-def newtons_method_sqrt(num, estimate):
-    # We need to apply the 'Newtons method formula', to create a new_estimate.
-    new_estimate= ((num/estimate)+ estimate)/ 2
+def is_close(num1, num2, tolerance=0.001): 
+    return abs(num1 - num2) < tolerance
+
+def newtons_method_sqrt(num, estimate): # We need to apply the 'Newtons method formula', to create a new_estimate.
+    new_estimate = (((num/estimate)+ estimate)/ 2) 
     # We can now use a 'Built in Function', the 'if' statement to compare our estimates.
-    
-    if new_estimate != m.sqrt(num):
-        return newtons_method_sqrt(num, new_estimate)
-    elif new_estimate == m.sqrt(num):
+
+    if is_close(estimate, new_estimate):
         approx_sqrt = (new_estimate)
         return approx_sqrt
-        
-
-# Check point values 'newtons_method_sqrt(20,4)'
+    else:
+        return newtons_method_sqrt(num, new_estimate)
 
 original_num= (int(input('Please enter the number you wish to retrieve the square root of: ')))
 
