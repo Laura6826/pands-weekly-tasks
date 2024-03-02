e91eaa75e8b3457a6dd10be3ee1836f4a7bf9201
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-02 09:05:25 +0000

    updated global variables

--- week02/bank.py
+++ week02/bank.py
@@ -3,13 +3,15 @@
 # add the two ammounts and print the answer.
 # Author: Laura Lyons
 
-x = int(input('Enter ammount 1 (in cents): ')) 
+ammount1 = int(input('Enter ammount 1 (in cents): '))  # Initally, i called this variable 'x' however, 
+# after learning about global varialbes, i will re-name it ammount1 (similar for ammount2)
+
 
 # input reads in a string so we need to convert to an integer
 # to allow us to perform mathematical operations.   
 
-y = int(input('Enter ammount 2 (in cents): '))
-answer= ((x+y)/100) # By dividing by 10, i will convert from cents to euro
+ammount2 = int(input('Enter ammount 2 (in cents): '))
+answer= ((ammount1+ammount2)/100) # By dividing by 10, i will convert from cents to euro
 print (f'The sum of these is: \N{euro sign}{answer}')
 
 # I pulled the coding for the euro symbol: \N{euro sign}, from stackoverflow.com 
