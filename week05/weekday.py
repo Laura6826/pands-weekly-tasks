8641bec11f8d51ae14f336d3a40a0ea8768c800b
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-02 08:24:25 +0000

    updated README weekly task o5

--- week05/weekday.py
+++ week05/weekday.py
@@ -4,14 +4,15 @@
 
 from datetime import date # I searchted the internet on how generate what the current day is. 
 # The datetime module provides classes for manipulating dates and times in Python.
+# Reference: https://docs.python.org/3/library/datetime.html 
+# Reference: https://www.w3schools.com/python/python_datetime.asp
 
-current_date = date.today() # We use this code to get the current date.
-day_of_week = current_date.weekday() # This code is used to tell us the specific day of the week,  (0 = Monday, 6 = Sunday)
+current_day = date.today() # We use this code to get the current date.
+day_of_week = current_day.weekday() # This code is used to tell us the specific day of the week,  (0 = Monday, 6 = Sunday)
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] # This is a list of the weekday names.
 
-while weekdays [:6]: # The range is used to indicate which days are weekdays.
+if day_of_week is weekdays[:5]: # The range is used to indicate which days are weekdays.
     print(f"Today is {weekdays[day_of_week]}.\nUnfortunately this is a weekday, go back to work!.")
-    break #It is necessary to insert a 'break' here, otherwise the program will run indefinitely, until physically prompted to stop.
 else:
     print(f'Today is {weekdays[day_of_week]}.\nHurrah, go back to bed!')
 
