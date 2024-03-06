42ead892b188ee5e5b889ac0aa4fca70e03e2d43
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-06 18:00:25 +0000

    edited code as it wasnt correct

--- week05/weekday.py
+++ week05/weekday.py
@@ -7,12 +7,22 @@ from datetime import date # I searchted the internet on how generate what the cu
 # Reference: https://docs.python.org/3/library/datetime.html 
 # Reference: https://www.w3schools.com/python/python_datetime.asp
 
-current_day = date.today() # We use this code to get the current date.
-day_of_week = current_day.weekday() # This code is used to tell us the specific day of the week,  (0 = Monday, 6 = Sunday)
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] # This is a list of the weekday names.
+current_day = date.today()          # We use this code to get the current date.
+day_of_week = current_day.weekday() # This code is used to tell us the specific day of the week,  (0 = Monday, sunday= 6)
+name_of_day= ('Monday', 
+              'Tuesday', 
+              'Wednesday', 
+              'Thursday', 
+              'Friday', 
+              'Saturday', 
+              'Sunday')
+today= (name_of_day[day_of_week])
 
-if day_of_week is weekdays[:5]: # The range is used to indicate which days are weekdays.
-    print(f"Today is {weekdays[day_of_week]}.\nUnfortunately this is a weekday, go back to work!.")
+weekday= today[:5]
+
+for today in weekday:               # The range is used to indicate which days are weekdays.
+    print(f"Today is {(name_of_day[day_of_week])}.\nUnfortunately this is a weekday, go back to work!.")
+    break
 else:
-    print(f'Today is {weekdays[day_of_week]}.\nHurrah, go back to bed!')
-
+    print(f"Today is {name_of_day[day_of_week]}.\nIts the weekend! Hurrah, go back to bed!")
+    
