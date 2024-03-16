d4c4de439fa9bd5f4938d9c2702f009bb99184ed
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-16 06:29:47 +0000

    updated weekday.py

--- week05/weekday.py
+++ week05/weekday.py
@@ -8,20 +8,16 @@ from datetime import date # I searchted the internet on how generate what the cu
 # Reference: https://www.w3schools.com/python/python_datetime.asp
 
 current_day = date.today()          # We use this code to get the current date.
-day_of_week = current_day.weekday() # This code is used to tell us the specific day of the week,  (0 = Monday, sunday= 6)
-name_of_day= ('Monday',             # A tuple was created to allow us create an index for the day of the week.
+day_of_week = current_day.weekday() # This code is used to tell us the specific day of the week, as a number/integer.(0 = Monday, sunday= 6)
+name_of_day= ('Monday',             # A tuple was created as an index for the day of the week.
               'Tuesday', 
               'Wednesday', 
               'Thursday', 
               'Friday', 
               'Saturday', 
-              'Sunday')
+              'Sunday')         
 
-today= (name_of_day[day_of_week])
-
-weekday= today [0:5]                 # The range is used to indicate which days are weekdays.              
-
-if weekday:  
+if day_of_week <5:  
     print(f"Today is {(name_of_day[day_of_week])}.\nUnfortunately this is a weekday, go back to work!.")
     #break                           # This will ensure the code only runs once
 else:
