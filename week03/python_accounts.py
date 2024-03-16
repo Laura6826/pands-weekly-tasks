96ca26914abdf6fd48003d4046be66fe61541013
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-16 07:25:18 +0000

    updated weekly task 3

--- week03/python_accounts.py
+++ week03/python_accounts.py
@@ -3,20 +3,20 @@
 # the account number, with the first 6 digits replaced with 'x' only the last four digits visible.
 # Author: Laura Lyons
 
-account_number = input('Please enter a 10 digit account number: ')
-
-clean_account_number = account_number.strip()
+account_number = input('Please enter a 10 digit account number: ').strip()
 #  the strip() comand was used, to remove any uncessary white space.
 
-hidden_account_number = 'x' * 6 + clean_account_number [6:]
+hidden_account_number = 'x' * 6 + account_number [6:]
 # the splicing code is used to highlight the numbers that we want to be replaced by x's.
 
-if len(clean_account_number) == 10 and clean_account_number.isdigit(): 
-    # Print the final number 'if' the entered number contains 10 intgers only
+while len(account_number) != 10 and not account_number.isdigit(): 
+    # Print the final number if the entered number contains 10 intgers only
     # Reference: w3schools https://www.w3schools.com/python/ref_stringisdigit.asp 
-    print(hidden_account_number)
+    account_number= input('Invalid account number entered. Please enter a valid 10 digit number.')
+    hidden_account_number = 'x' * 6 + account_number [6:]
 else:
-    print ('Invalid account number entered. Please enter a valid 10 digit number.')
+    print(hidden_account_number)
+    
    
 
 
